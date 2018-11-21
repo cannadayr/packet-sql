755ea76e1be298c9d9d9443dc9f5b697fa6e5ce7
Rowan Cannaday <cannadayr@gmail.com>
2018-11-21 14:55:35 -0800

    start to generalize plotting script
    fix readme fmt

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ```
 
 ## Optional Dependencies
-maxmind database for city lookup
-libgd-dev
-https://www.caida.org/tools/visualization/plot-latlong/
-libgd-perl
+* maxmind database for city & coord lookup
+* libgd-dev
+* libgd-perl
+* https://www.caida.org/tools/visualization/plot-latlong/ for plotting points to an image
--- plot.sh
+++ plot.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+#map=World # default
+map=USA200 # higher res USA
+#pixels=10 # default plot size
+pixels=20
 ./info.sh \
     | awk -F ", " '{print $7 " " $8}' \
-    | plot-latlong-0.3/plot-latlong -s 10 -i plot-latlong-0.3/.mapinfo > plot.png
+    | plot-latlong-0.3/plot-latlong -m ${map} -s ${pixels} -i plot-latlong-0.3/.mapinfo > images/${map}.png
